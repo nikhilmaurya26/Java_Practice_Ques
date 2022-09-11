605cafbe230c7365e16407732aea7da7eae40f8d
Nikhil <nikhilsingh0926@gmail.com>
2022-09-11 23:58:28 +0530

    Fibonacci

--- Sumofdigit.java
+++ Sumofdigit.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class fabonaci {
+public class Sumofdigit {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
