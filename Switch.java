d6734a4f2676d2463f326ed86adc10c9cdc3b7e4
Nikhil <nikhilsingh0926@gmail.com>
2022-09-29 00:06:16 +0530

    Switch_cases

---
+++ Switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String  fruit = sc.next();
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("Keep the doctor away");
+                break;
+            case "Grapes":
+                System.out.println("They were sour");
+                break;
+            case "kivi":
+                System.out.println("goof for blood");
+                break;
+            default:
+                System.out.println("enter a valid fruit ");
+                break;
+        }
+    }
+}
