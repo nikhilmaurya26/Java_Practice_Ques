eba4f089c383f1ab8c5cd704b49c6b0e3a2b447b
Nikhil <nikhilsingh0926@gmail.com>
2022-09-29 00:09:30 +0530

    new_switch_statement

--- Switch.java
+++ Switch.java
@@ -5,21 +5,25 @@ public class Switch {
         Scanner sc = new Scanner(System.in);
         String  fruit = sc.next();
         switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("Keep the doctor away");
-                break;
-            case "Grapes":
-                System.out.println("They were sour");
-                break;
-            case "kivi":
-                System.out.println("goof for blood");
-                break;
-            default:
-                System.out.println("enter a valid fruit ");
-                break;
+            case "mango" ->  System.out.println("King of fruits");
+            case "orange" ->  System.out.println("King of fruitsssssss");
+            default -> System.out.println("not valid");
+
+            // case "Mango":
+            //     System.out.println("King of fruits");
+            //     break;
+            // case "Apple":
+            //     System.out.println("Keep the doctor away");
+            //     break;
+            // case "Grapes":
+            //     System.out.println("They were sour");
+            //     break;
+            // case "kivi":
+            //     System.out.println("goof for blood");
+            //     break;
+            // default:
+            //     System.out.println("enter a valid fruit ");
+            //     break;
         }
     }
 }
