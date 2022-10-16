bafd5b914e5f98bd959146ddffcc688d01291f45
Nikhil <nikhilsingh0926@gmail.com>
2022-10-16 17:35:07 +0530

    array_folder

---
+++ ARRAYS/array1.java
@@ -0,0 +1,27 @@
+package ARRAYS;
+import java.util.*;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size");
+        int size = sc.nextInt();
+        
+        int[] arr = new int[size];
+
+        for(int i = 0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0 ; i < size ; i++){
+            if(arr[i]==size){
+                System.out.println(size);
+                break;
+                
+            }
+            else{
+                System.out.println("Not found");
+                break;
+            }
+        }
+    }
+}
